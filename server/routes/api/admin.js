109b755823c7d90bf450007a03a472276fced8e7
Johnny Richardson <johnny.c.rich@gmail.com>
2020-06-17 14:04:04 -0400

    feat(server): get most recent progress (survey) date for every user in /api/admin/get/users

--- server/routes/api/admin.js
+++ server/routes/api/admin.js
@@ -19,21 +19,47 @@ exports.users = async (req, res) => {
 
     let usersFind = AppUser.find({}, 'name email lastLogin');
 
+
     try {
         let userRes = await usersFind.exec();
         await Promise.all(userRes.map(async (user) => {
 
-            let userProjects = Project.find({user: user._id}, 'name description slug');
-            let p = await userProjects.exec();
+            // Get all user's projects
+            const projects = await Project.find({
+                user: user._id
+            }, 'name description slug').exec();
 
-            return Object.assign({}, {user: user, projects: p});
+            // Promise for all progress for all user's projects
+            return await Promise.all(projects.map(async (project) => {
 
-        })).then(results =>  {
-            // console.log
+                // Get latest progress for this project
+                const projProgress = await Progress.findOne({
+                    project: project._id
+                }, 'date -_id').sort({
+                    'date': -1
+                }).exec();
+                return !projProgress ? null : projProgress.date;
+
+            })).then((progress) => {
+
+                // Get most recent progress (survey) date
+                const latestSurveyDate = progress.sort((a, b) => {
+                    return new Date(b) - new Date(a);
+                })[0];
+
+                // Create object for this user
+                return Object.assign({}, {
+                    user,
+                    projects,
+                    latestSurveyDate
+                });
+
+            });
+
+        })).then(results => {
             res.status(200).send(results)
         });
-    }
-    catch(e) {
+    } catch (e) {
         console.error(e);
         res.status(500).send(e);
     }
@@ -45,16 +71,22 @@ exports.users = async (req, res) => {
 exports.project = async (req, res) => {
 
 
-    let getProject = Project.findOne({_id: req.params.projectId}, 'name description user _id').populate('user');
+    let getProject = Project.findOne({
+        _id: req.params.projectId
+    }, 'name description user _id').populate('user');
 
     try {
         let getProjectRes = await getProject.exec();
-        let projProgress = Progress.find({project: getProjectRes._id}, 'sumX sumY note date -_id');
+        let projProgress = Progress.find({
+            project: getProjectRes._id
+        }, 'sumX sumY note date -_id');
         let getProgressRes = await projProgress.exec();
 
-        res.json({project: getProjectRes, progress: getProgressRes});
-    }
-    catch(e) {
+        res.json({
+            project: getProjectRes,
+            progress: getProgressRes
+        });
+    } catch (e) {
         console.error(e);
         res.sendStatus(404);
     }
